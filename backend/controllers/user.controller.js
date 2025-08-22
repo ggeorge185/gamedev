658635ae1c98dde130ff92c28ea718d1b5c172ce
sreeni7799 <sreenivas.shenoy@ovgu.de>
2025-08-22 15:33:17 +0200

    jwt-error-issue

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -57,7 +57,7 @@ export const login = async (req, res) => {
             });
         };
 
-        const token = await jwt.sign({ userId: user._id }, process.env.SECRET_KEY, { expiresIn: '1d' });
+        const token = await jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
 
         
         user = {
