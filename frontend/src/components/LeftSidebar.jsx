ad4b75ed3fc9c82a7fac390ac3972e7df88b6ebf
Gabriel George <56782229+ggeorge185@users.noreply.github.com>
2025-08-24 15:58:17 +0200

    Refactor LeftSidebar component layout and structure

--- frontend/src/components/LeftSidebar.jsx
+++ frontend/src/components/LeftSidebar.jsx
@@ -1,10 +1,9 @@
-import { Home, LogOut, PlusSquare, Search, BookOpen, User } from 'lucide-react'
+import { Home, LogOut, PlusSquare, Search, BookOpen } from 'lucide-react'
 import React, { useState } from 'react'
-import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
-import { toast } from 'sonner'
-import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
+import { toast } from 'sonner'
+import axios from 'axios'
 import { setAuthUser } from '@/redux/authSlice'
 import AddWordForm from './AddWordForm'
 import { setWords } from '@/redux/wordSlice'
@@ -54,26 +53,23 @@ const LeftSidebar = () => {
     ]
 
     return (
-        <div className='fixed top-0 z-10 left-0 px-4 border-r border-gray-300 w-[16%] h-screen bg-white'>
-            <div className='flex flex-col'>
-                <h1 className='my-8 pl-3 font-bold text-xl text-blue-600'>Serious Game admin board</h1>
-                <div>
-                    {sidebarItems.map((item, index) => {
-                        return (
-                            <div onClick={() => sidebarHandler(item.text)} key={index} className='flex items-center gap-3 relative hover:bg-gray-100 cursor-pointer rounded-lg p-3 my-3'>
-                                {item.icon}
-                                <span>{item.text}</span>
-                            </div>
-                        )
-                    })}
-                </div>
+        <aside className='w-64 h-screen bg-white border-r border-gray-300 px-4 flex flex-col'>
+            <h1 className='my-8 pl-3 font-bold text-xl text-blue-600'>Serious Game admin board</h1>
+            <div>
+                {sidebarItems.map((item, index) => (
+                    <div
+                        onClick={() => sidebarHandler(item.text)}
+                        key={index}
+                        className='flex items-center gap-3 relative hover:bg-gray-100 cursor-pointer rounded-lg p-3 my-3'
+                    >
+                        {item.icon}
+                        <span>{item.text}</span>
+                    </div>
+                ))}
             </div>
-
             <AddWordForm open={open} setOpen={setOpen} />
-        </div>
+        </aside>
     )
 }
 
 export default LeftSidebar
-
-
